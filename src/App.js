4f7c8e5ced6c4b77fcacd5f5dd0c95ebdd2563d7
Yago Ernandes <yago700@gmail.com>
2020-01-10 10:05:25 -0300

    Mock

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './style/main.less';
+
+export default class Welcome extends React.Component {
+	render () {
+		return <h1>Hello World from Yago</h1>;
+	}
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style/main.less';
 
-class Welcome extends React.Component {
-	render () {
-		return <h1>Hello World from React boilerplate</h1>;
-	}
-}
+import App from './App';
 
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
