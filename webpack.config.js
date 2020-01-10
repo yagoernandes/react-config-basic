2b1e57cde757544c31c6aedcf15a2e038f096350
Yago Ernandes <yago700@gmail.com>
2020-01-10 09:33:27 -0300

    Configuração do webpack para o less

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,29 @@ module.exports = {
 	devtool: 'inline-source-map',
 	entry: './src/index.js',
 	output: {
-		path: __dirname + '/dist',
+		path: __dirname + '/build',
 		publicPath: '/',
 		filename: 'bundle.js'
 	},
 	devServer: {
-		contentBase: './dist',
+		contentBase: './build',
 	},
 	module: {
 		rules: [{
 			test: /\.(js|jsx)$/,
 			exclude: /node_modules/,
-			use: ['babel-loader', 'eslint-loader']
-		}]
+			use: [
+				'babel-loader',
+				'eslint-loader'
+			]
+		},
+		{
+			test: /\.less$/,
+			use: ['style-loader',
+				'css-loader',
+				'less-loader',
+			],
+		},
+		]
 	},
 };
