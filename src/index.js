1bcf10302ce5daecc2499354f2516320c4ae3737
Yago Ernandes <yago700@gmail.com>
2020-01-10 09:41:56 -0300

    Mock

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style/main.less';
 
 class Welcome extends React.Component {
-	render() {
-		return <h1>Hello World from React boilerplate</h1>
+	render () {
+		return <h1>Hello World from React boilerplate</h1>;
 	}
 }
 
-ReactDOM.render(<Welcome />, document.getElementById("root"));
+ReactDOM.render(<Welcome />, document.getElementById('root'));
